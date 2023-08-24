9d5e86970b58d40df04e0dc66c9ef162057835a2
Mike Zarandona <mike.zarandona@gmail.com>
2023-08-24 19:23:23 -0400

    Configs + locks

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.plex.direct",
+        pathname: "/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
